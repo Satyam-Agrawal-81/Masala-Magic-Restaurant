587b6145e9a5b5659b3b9eb8c6fde485900b878a
Satyam-Agrawal-81 <ujnsatyamujn@gmail.com>
2023-09-06 03:40:38 +0530

    Second commit

---
+++ src/Components/Basics/MenuCard.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const MenuCard = ({menuData}) => {
+    // console.log(menuData)
+    // const myStyle={color:"brown"};//an object is created in JS
+    return (
+        <> 
+        <section className='main-card--cointainer'>
+        {
+            menuData.map((curElem)=>{
+
+                //destructuring so same piece of code me bar bar curElem likhne ki need nahi he 
+
+       const {id, name,category,image,description}=curElem;
+
+               return(
+                <>
+               {/* <div className="card-container" key={curElem.id}> */}
+               {/* After destructuring we don't need to write curElem.id instead write id only */}
+               <div className="card-container" key={id}> 
+               
+              <div className="card">
+                <div className="card-body">
+                <span className="card-number card-circle subtle">{id}</span>
+                <span className="card-author subtle" style={{color:"red"}}>{category}</span>
+                <h2 className='card-title'>{name}</h2>
+                <span className='card-description subtle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolorum obcaecati fugiat est odio architecto porro omnis facere hic temporibus.</span>
+                <div className='card-read'>Read</div>
+    
+            </div>
+            <img src={image} alt="images" className='card-media'/>
+    
+            <span className='card-tag subtle' style={{color:"Black"}}>Order Now</span>
+    
+        </div>
+        </div>    
+    
+                </>
+               ) 
+            })
+        }
+        </section>
+        
+
+
+     
+        </>
+      )
+}
+
+export default MenuCard;
---
+++ src/Components/Basics/Restaurant.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import "./style.css";
+import Menu from "./menuApi.js"
+import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+
+const uniqueList =  [ ... new Set( Menu.map((curr)=>{
+  return curr.category;
+})
+),
+"All"
+];
+console.log(uniqueList);
+
+const Restaurant = () => {
+  const [menuData, setMenuData] = React.useState(Menu);
+  const[menuList,setMenuList] =React.useState(uniqueList);
+
+  const filterItem = (category)=>{
+
+   if(category==="All"){
+    setMenuData(Menu);
+    return;
+   }
+
+    const updatedList = Menu.filter((curr) =>{
+      return curr.category === category;
+    }); // particular categort k type se hi milega and navbar me show karega then accordingly food of category choosen 
+    setMenuData(updatedList);
+  };
+
+return(
+  <>
+  {/* <nav className='navbar'>
+<div className='btn-group'>
+<button className='btn-group__item' onClick={()=>filterItem("breakfast")}>Breakfast</button>
+<button className='btn-group__item' onClick={()=>filterItem("lunch")}>Lunch</button>
+<button className='btn-group__item' onClick={()=>filterItem("evening")}>Evening</button>
+<button className='btn-group__item' onClick={()=>filterItem("dinner")}>Dinner</button>
+<button className='btn-group__item' onClick={()=>setMenuData(Menu)}>All</button>
+</div>
+  </nav> */}
+  {/* This is written in other js file Navbar.js for looping components instead of DRY */}
+   
+   < Navbar filterItem={filterItem}  menuList={menuList}/>
+   {/* two props are passsed in above statement */}
+   <MenuCard  menuData={menuData}/>
+  </>
+);
+  
+};
+
+export default Restaurant
+// default wala part EC6 me aa chuka he update to isliye App.js m import kari restaurant ki file
